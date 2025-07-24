ecfcc84c18a2e72f474eb3e03b5175e73ce014af
Triaguin <178437649+TriaguinDev@users.noreply.github.com>
2025-07-24 05:08:22 -0300

    24-07
    1 - Criei uma pasta para elementos que serão usado no app, caso queiram organizar melhor elementos como imagem e etc..
    
    2- Criei um arquivo que irá guardar as paletas usadas no aplicativo

---
+++ Elementos/Paleta.js
@@ -0,0 +1,10 @@
+export default {
+    /* Paleta de cores do App*/
+
+    /*Cores principais do app */
+    AzulEscuro: " #021B33", 
+    Azul: " #12577B", 
+    Branco: " #F7F7F7",
+    AzulClaro: " #A9CFE5"
+
+};
