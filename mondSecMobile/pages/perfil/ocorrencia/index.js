5afee1d2337d81ff2070dab7345e02b1b4f5495f
unknown <hdtmrsame8@gmail.com>
2025-08-27 21:20:59 -0300

    feat:Adicionando modal de detalhes ocorrencia

--- mondSecMobile/pages/perfil/ocorrencia/index.js
+++ mondSecMobile/pages/perfil/ocorrencia/index.js
@@ -1,13 +1,13 @@
 import React, {useState, useEffect} from "react";
-import {Pressable, View, Button, Text} from 'react-native';
+import {Pressable, View, Button, Text, TextInput, Modal, StyleSheet} from 'react-native';
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { TextInput } from "react-native";
 import axios from "axios";
 
 const OcorrenciaScreen = ({navigation}) => {
     const [ocorrencias, setOcorrencias] = useState([]);
     const [lengthNumber, setLenghtNumber] = useState('');
-
+    const [informacao, setInformacao] = useState(false);
+    const [indice, setIndice] = useState('');
     useEffect(() => {
 
         buscarOcorrencia();
@@ -23,7 +23,7 @@ const OcorrenciaScreen = ({navigation}) => {
                 return;
             }
             
-            const {data} = await axios.get('http://127.0.0.1:8000/api/procurar', {
+            const {data} = await axios.get('http://127.0.0.1:8000/api/ocorrencia/procurar', {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
@@ -42,6 +42,16 @@ const OcorrenciaScreen = ({navigation}) => {
         }
     }
 
+    const mostrarModal = () => {
+
+        setInformacao(true);
+    }
+
+    const desaparecer = async () => {
+        await setInformacao(false);
+        navigation.navigate('Home');
+
+    }
     return (
         <View>
             <View>
@@ -61,15 +71,68 @@ const OcorrenciaScreen = ({navigation}) => {
                 </Pressable>
                 {ocorrencias.map((ocorrencia, index) => (
                     <View key={index}>
-                        <Text>{ocorrencia.tituloOcorrencia}</Text>
-                        <Text>{ocorrencia.longitudeOcorrencia}</Text>
-                        <Text>{ocorrencia.latitudeOcorrencia}</Text>
-                        <Text>{ocorrencia.dataRegistradaOcorrencia}</Text>
-                        <Button/>
+                        <Text>{ocorrencia.titulo}</Text>
+                        <Text>{ocorrencia.longitude}</Text>
+                        <Text>{ocorrencia.latitude}</Text>
+                        <Text>{ocorrencia.data}</Text>
+                        <Pressable onPress={() => {setIndice(index); mostrarModal();}}>
+                            <Text>Ver detalhes</Text>
+                        </Pressable>
                     </View>
-                    ))}
+                ))}
             </View>
+            <Modal animationType="slide" visible={informacao} transparent>
+                {indice != null && ocorrencias[indice] && (
+                    <View style={styles.modalContainer}>
+                        <View style={styles.modalContent}>
+                            <Text style={styles.modalTitle}>Detalhes</Text>
+                                <Text style={styles.modalText}>{ocorrencias[indice].titulo}</Text>
+                                <Text style={styles.modalText}>{ocorrencias[indice].longitude}</Text>
+                                <Text style={styles.modalText}>{ocorrencias[indice].latitude}</Text>
+                                <Text style={styles.modalText}>{ocorrencias[indice].data}</Text>
+                                <Text style={styles.modalText}>{ocorrencias[indice].descricao}</Text>
+                                <Pressable onPress={() => desaparecer()}>
+                                    <Text style={styles.modalButton}>Ver no Mapa</Text>
+                                </Pressable>
+                                <Pressable onPress={() => setInformacao(false)}>
+                                    <Text style={styles.modalButton}>Favoritar</Text>
+                                </Pressable>
+                        </View>
+                    </View>
+                )}
+            </Modal>
         </View>
     );
 };
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: '#FFFFFF',
+    padding: 20,
+    borderRadius: 10,
+    width: '90%',
+    alignItems: 'center',
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#003366',
+  },
+  modalText: {
+    textAlign: 'center',
+    marginBottom: 20,
+    color: '#333',
+  },
+  modalButton: {
+    color: '#007BFF',
+    marginTop: 10,
+    fontWeight: 'bold',
+  },
+});
 export default OcorrenciaScreen;
