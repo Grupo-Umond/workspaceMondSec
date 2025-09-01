89d5ea4a812bc4db3db4db71c8d634d739b93beb
Pedro <phrds71@gmail.com>
2025-09-01 17:35:26 -0300

    Estilização da tela do cadastro dos adms

--- mondSecWeb/resources/views/siteAdm/verUser/index.blade.php
+++ mondSecWeb/resources/views/siteAdm/verUser/index.blade.php
@@ -3,9 +3,30 @@
 <head>
     <meta charset="UTF-8" />
     <title>Usuarios Cadastrados</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+        <link rel="stylesheet" href="{{ asset('css/15usuariosCadastrados.css') }}">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 <body class="bg-light">
+
+    <header>
+        <nav>
+            <a href=""> <i class="fa-solid fa-house"></i> DashBoard</a>
+            <a href=""> <i class="fa-solid fa-chart-simple"></i> Avaliações</a>
+            <a href=""> <i class="fa-solid fa-comments"></i> Chat</a>
+            <a href=""> <i class="fa-solid fa-magnifying-glass"></i> Status</a>
+            <a href=""> <i class="fa-solid fa-magnifying-glass"></i> Views</a>
+            <a href=""> <i class="fa-solid fa-magnifying-glass"></i> Transações</a>
+            <a href="{{ route('adm.showadm') }}" class="btn btn-primary btn-lg flex-fill">Adiministradores</a>
+            <a href="{{ route('adm.showuser') }}" class="btn btn-secondary btn-lg flex-fill">Usuários</a>
+            <a href=""> <i class="fa-solid fa-gear"></i> Configurações</a>
+        </nav>
+    </header>
+
     <div class="container py-5">
         <h1 class="mb-4">Usuarios Cadastrados</h1>
         @method(`DELETE`)
@@ -47,7 +68,9 @@
             </table>
         @endif
 
-        <a href="{{ route('adm.dashboard') }}" class="btn btn-secondary mt-4">Voltar ao Painel</a>
+                <a href="{{ route('adm.dashboard') }}" class="link-btn">
+            <div class="botao">Voltar ao Painel</div>
+        </a>
     </div>
 </body>
 </html>
