487585f1ce7c1252b0b87f12aa5b842cdaa65685
Pedro <phrds71@gmail.com>
2025-10-07 20:26:24 -0300

    Nova estilização da tela de login e cadastro do adm e implementação dos gráficos

--- mondSecWeb/resources/views/adm/auth/cadastro.blade.php
+++ mondSecWeb/resources/views/adm/auth/cadastro.blade.php
@@ -1,122 +1,97 @@
+
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
-    <title>Cadastro</title>
 
-    <link rel="stylesheet" href="{{ asset('css/11cadastroADM.css') }}">
+    <title>Login</title>
 
+    <link rel="stylesheet" href="{{ asset('css/09loginADM.css') }}">
 
 </head>
+
 <body>
 
-    <header>
+    <div class="parteEsquerda">
 
-        <nav>
+        <div class="logo">
+                <img src="{{ asset('Imagens/Logos/umondlogoazul.png') }}" class="logo" alt="Logo">
+        </div>
 
-            <h2> MONDSEC </h2>
+        <div class="container">
+            <h2>Entrar</h2>
 
-            <h2> ADMINISTRADOR </h2>
+            <form id="formLogin" action="{{ route('adm.auth.login.store') }}" method="POST">
+                @csrf
 
-        </nav>
+                <div class="input-grupo1">
+                    <input required class="campoEmail" type="email" id="email" name="email" value="{{ old('email') }}">
+                    <label class="emailLabel" for="email">
+                        Email:
+                    </label>
+                    @error('email')
+                        <div class="error">{{ $message }}</div>
+                    @enderror
+                </div>
 
-    </header>
+                <div class="input-grupo2">
+                    <input class="campoSenha" type="password" id="senha" name="senha" required>
+                    <label class="senhaLabel" for="senha">Senha:</label>
+                    @error('senha')
+                        <div class="error">{{ $message }}</div>
+                    @enderror
+                </div>
 
-    <div class="container">
-        <h2>Cadastro de Administrador</h2>
+                <button type="submit">Entrar</button>
+            </form>
 
-        @if(session('success'))
-            <div class="success">{{ session('success') }}</div>
-        @endif
-
-        <form id="formCadastro" action="{{ route('adm.auth.register.store') }}" method="POST">
-            @csrf
-
-            <div class="input-grupo1">
-            <input required class="campoNome" type="text" id="nome" name="nome" value="{{ old('nome') }}">
-            <label class="nomeLabel" for="nome">Nome:</label>
-            @error('nome')
-                <div class="error">{{ $message }}</div>
-            @enderror
+        </div>
+        <div class="adicionais">
+            <p>
+                Copyright ©2025 MondSec, L.L.C MondSec é um produto de mondSec, L.L.C.
+            </p>
+            <div class="termos">
+                <p>
+                    termos de servico
+                </p>
+                <p> | </p>
+                <p>
+                    termos de privacidade
+                </p>
             </div>
-
-            <div class="input-grupo2">
-            <input required class="campoEmail" type="email" id="email" name="email" value="{{ old('email') }}">
-            <label class="emailLabel" for="email">Email:</label>
-            @error('email')
-                <div class="error">{{ $message }}</div>
-            @enderror
-            </div>
-
-            <div class="input-grupo3">
-            <input required class="campoTelefone" type="text" id="telefone" name="telefone" value="{{ old('email') }}">
-            <label class="telefoneLabel" for="email">Telefone:</label>
-            @error('email')
-                <div class="error">{{ $message }}</div>
-            @enderror
-            </div>
-
-            <div class="input-grupo4">
-            <input required class="campoSenha" type="password" id="senha" name="senha">
-            <label class="senhaLabel" for="senha">Senha:</label>
-            @error('senha')
-                <div class="error">{{ $message }}</div>
-            @enderror
-            </div>
-
-            <div class="input-grupo5">
-            <input required class="campoConfirmarSenha" type="password" id="senhaConfirma" name="senhaConfirma">
-            <label class="senhaConfirmarLabel" for="senhaConfirma">Confirme a Senha:</label>
-            </div>
-
-            <div class="select-grupo6">
-            <label class="acessoLabel" for="nivelAdmin">Nível de Acesso:</label>
-            <select required class="campoAcesso" id="nivelAdmin" name="nivelAdmin">
-                <!-- <option value="">Nível de acesso</option> -->
-                <option value="ouro" {{ old('nivel_acesso') == 'ouro' ? 'selected' : '' }}>Ouro</option>
-                <option value="prata" {{ old('nivel_acesso') == 'prata' ? 'selected' : '' }}>Prata</option>
-                <option value="bronze" {{ old('nivel_acesso') == 'bronze' ? 'selected' : '' }}>Bronze</option>
-            </select>
-            @error('nivel_acesso')
-                <div class="error">{{ $message }}</div>
-            @enderror
-            </div>
-
-            <button type="submit">Cadastrar</button>
-        </form>
+        </div>
+    </div>
+    <div class="parteDireita">
+        <video controls autoplay muted loop>
+            <source src="{{ asset('Videos/video3.mp4') }}" type="video/mp4">
+        </video>
     </div>
     <script>
-        const form = document.getElementById('formCadastro');
+        const form = document.getElementById('formLogin');
 
-        form.addEventListener('submit', function(event) {
-            const nome = document.getElementById('nome').value;
-            const senha = document.getElementById('senha').value;
-            const confirmaSenha = document.getElementById('senhaConfirma').value;
-            const email = document.getElementById('email').value;
-            const nivelAdmin = document.getElementById('nivelAdmin').value;
-            const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        form.addEventListener('submit', function (event) {
+            const email = document.getElementById('email');
+            const senha = document.getElementById('senha');
 
-            if(!nome || !senha || !confirmaSenha || !email || !nivelAdmin) {
+            if (!email || !senha) {
                 event.preventDefault();
-                alert('Preencha todos os campos')
-            }
-            if(senha !== confirmaSenha) {
-                event.preventDefault();
-                alert('As senha não coincidem. Por favor, corrija.');
+                alert('Preencha os campos obrigatorios');
             }
 
-            if(!regex.test(email)) {
+            if (!regex.test(email)) {
                 event.preventDefault();
                 alert('Email invalido');
             }
 
-            if(senha.length < 8 || confirmaSenha.length < 8) {
+            if (senha.length < 8) {
                 event.preventDefault();
-                alert('Senha deve ter mais de 8 digitos');
+                alert('Digite uma senha com no minimo 8 digitos');
             }
 
 
         });
     </script>
 </body>
+
 </html>
--- mondSecWeb/resources/views/adm/verAdm/index.blade.php
+++ mondSecWeb/resources/views/adm/verAdm/index.blade.php
@@ -1,63 +1,214 @@
 @extends('adm.layouts.admin')
 
-@section('title','View')
+@section('title', 'View')
 
 @section('content')
     <div class="container py-5">
-        <h1 class="mb-4">Admins Cadastrados</h1>
-        @method(`DELETE`)
-        @if($admins->isEmpty())
+        <div class="parteCima">
+            <h1 class="mb-4">Admins Cadastrados</h1>
+            <div class="graficos">
+            
+                <style>
+                    .graficos {
+                        display: flex;
+                    }
+                </style>
+                <!-- <h2>ADM´s cadastrados</h2> -->
+            
+                    <div id="chart-container1"></div>
+                    <script src="https://echarts.apache.org/en/js/vendors/echarts/dist/echarts.min.js"></script>
+            
+                <style>
+                    * {
+                        margin: 0;
+                        padding: 0;
+                    }
+            
+                    #chart-container1 {
+                        /* position: relative; */
+                        height: 45vh;
+                        width: 50%;
+                        overflow: hidden;
+                    }
+                </style>
+            
+                <script>
+                    var dom = document.getElementById('chart-container1');
+                    var myChart = echarts.init(dom, null, {
+                        renderer: 'canvas',
+                        useDirtyRect: false
+                    });
+                    var app = {};
+            
+            
+                    var option;
+            
+                    option = {
+                        tooltip: {
+                            trigger: 'axis',
+                            axisPointer: {
+                                type: 'shadow'
+                            }
+                        },
+                        grid: {
+                            left: '3%',
+                            right: '4%',
+                            bottom: '3%',
+                            containLabel: true
+                        },
+                        xAxis: [
+                            {
+                                type: 'category',
+                                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                                axisTick: {
+                                    alignWithLabel: true
+                                }
+                            }
+                        ],
+                        yAxis: [
+                            {
+                                type: 'value'
+                            }
+                        ],
+                        series: [
+                            {
+                                name: 'Direct',
+                                type: 'bar',
+                                barWidth: '60%',
+                                data: [10, 52, 200, 334, 390, 330, 220]
+                            }
+                        ]
+                    };
+            
+                    if (option && typeof option === 'object') {
+                        myChart.setOption(option);
+                    }
+            
+                    window.addEventListener('resize', myChart.resize);
+                </script>
+            
+            
+                <div id="chart-container2"></div>
+                <script src="https://echarts.apache.org/en/js/vendors/echarts/dist/echarts.min.js"></script>
+            
+                <style>
+                    #chart-container2 {
+                        /* position: relative; */
+                        height: 45vh;
+                        width: 50%;
+                        overflow: hidden;
+                    }
+                </style>
+                <script>
+                    var dom = document.getElementById('chart-container2');
+                    var myChart = echarts.init(dom, null, {
+                        renderer: 'canvas',
+                        useDirtyRect: false
+                    });
+                    var app = {};
+            
+            
+                    var option;
+            
+                    option = {
+                        title: {
+                            text: '',
+                            subtext: '',
+                            left: 'center'
+                        },
+                        tooltip: {
+                            trigger: 'item'
+                        },
+                        legend: {
+                            orient: 'horizontal',
+                            left: 'center'
+                        },
+                        series: [
+                            {
+                                name: 'Access From',
+                                type: 'pie',
+                                radius: '50%',
+                                data: [
+                                    { value: 1048, name: 'Search Engine' },
+                                    { value: 735, name: 'Direct' },
+                                    { value: 580, name: 'Email' },
+                                    { value: 484, name: 'Union Ads' },
+                                    { value: 300, name: 'Video Ads' }
+                                ],
+                                emphasis: {
+                                    itemStyle: {
+                                        shadowBlur: 10,
+                                        shadowOffsetX: 0,
+                                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                                    }
+                                }
+                            }
+                        ]
+                    };
+            
+            
+                    if (option && typeof option === 'object') {
+                        myChart.setOption(option);
+                    }
+            
+                    window.addEventListener('resize', myChart.resize);
+                </script>
+            </div>
+            
+            @method(`DELETE`)
+            @if($admins->isEmpty())
             <div class="alert alert-warning">Nenhum admin cadastrado.</div>
-        @else
+            @else
             <table>
-                <thead class="table-dark">
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Email</th>
-                        <th>Telefone</th>
-                        <th>Nivel de Acesso</th>
-                        <th>Data de Criação</th>
-                        <th class="as1"></th>
-                        <th class="as"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($admins as $admin)
+                    <thead class="table-dark">
                         <tr>
-                            <td>{{ $admin->id }}</td>
-                            <td>{{ $admin->nome }}</td>
-                            <td>{{ $admin->email }}</td>
-                            <td>{{ $admin->telefone }}</td>
-                            <td>{{ $admin->nivelAdmin }}</td>
-                            <td>{{ $admin->created_at->format('d/m/Y H:i') }}</td>
-                            <td class="editarADM"><a href="{{ route('adm.admins.edit', $admin->id)}}">
-                                    <i class="fa-solid fa-pencil"></i>
-                                </a></td>
-                                
-                            <td class="excluirADM">
-                                <form action="{{ route('adm.admins.destroy', $admin->id) }}" method="POST"
-                                    onsubmit="return confirm('Tem certeza que quer excluir?');">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger btn-sm">
-
-                                        <i class="fa-solid fa-trash-can"></i>
-
-                                    </button>
-                                </form>
-                            </td>
-
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Email</th>
+                            <th>Telefone</th>
+                            <th>Nivel de Acesso</th>
+                            <th>Data de Criação</th>
+                            <th class="as1"></th>
+                            <th class="as"></th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        @endif
+                    </thead>
+                    <tbody>
+                        @foreach($admins as $admin)
+                            <tr>
+                                <td>{{ $admin->id }}</td>
+                                <td>{{ $admin->nome }}</td>
+                                <td>{{ $admin->email }}</td>
+                                <td>{{ $admin->telefone }}</td>
+                                <td>{{ $admin->nivelAdmin }}</td>
+                                <td>{{ $admin->created_at->format('d/m/Y H:i') }}</td>
+                                <td class="editarADM"><a href="{{ route('adm.admins.edit', $admin->id)}}">
+                                        <i class="fa-solid fa-pencil"></i>
+                                    </a></td>
 
-        <a href="{{ route('adm.dashboard.index') }}" class="link-btn">
-            <div class="botao">Voltar ao Painel</div>
-        </a>
+                                <td class="excluirADM">
+                                    <form action="{{ route('adm.admins.destroy', $admin->id) }}" method="POST"
+                                        onsubmit="return confirm('Tem certeza que quer excluir?');">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger btn-sm">
+
+                                            <i class="fa-solid fa-trash-can"></i>
+
+                                        </button>
+                                    </form>
+                                </td>
+
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
+            <a href="{{ route('adm.dashboard.index') }}" class="link-btn">
+    <div class="botao">Voltar ao Painel</div>
+</a>
+        </div>
+
+
     </div>
 </body>
 @endsection
-
