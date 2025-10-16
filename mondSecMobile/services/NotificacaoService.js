4522258281b2f36427b77194fd40a32d2a7b4a48
hdtmrs <hdtmrsame8@gmail.com>
2025-10-16 01:19:06 -0300

    feat:Adicionando pre requisitos par funcionamento de notificações no app

--- mondSecMobile/services/NotificacaoService.js
+++ mondSecMobile/services/NotificacaoService.js
@@ -1,5 +1,6 @@
 import * as Notifications from 'expo-notifications';
 import { Platform } from 'react-native';
+import UrlService from './UrlService';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -16,10 +17,9 @@ export async function NotificacaoService() {
   const token = (await Notifications.getExpoPushTokenAsync()).data;
   console.log('TOKEN DO DISPOSITIVO', token);
 
-  await axios.post('http://192.168.X.X:8000/api/notificacao/token', { token });
+  await UrlService.post('/notificacao/token', { token });
 
-  //Eae famlia, aqui ó, é pra testar a notificação(funciona só em mobile)
-  await axios.post('http://127.0.0.1:8000/api/notificacao/enviar', {
+  await UrlService.post('/notificacao/enviar', {
     token: expoToken,
     title: '🚨 Alerta de segurança',
     body: 'Algo foi detectado no sistema.'
