2f2ebdbc968e45360de025a0bab83b900fc0de48
hdtmrs <hdtmrsame8@gmail.com>
2025-10-29 00:01:24 -0300

    feat:adicionando tela de alteração da ocorrencia

---
+++ mondSecWeb/resources/views/adm/verOcorrencia/update.blade.php
@@ -0,0 +1,52 @@
+@extends('adm.layouts.admin')
+
+@section('title','Update')
+
+@section('content')
+
+<body>
+    <div class="container">
+        <h2>Alterar Dados da Ocorrencia</h2>
+
+        @if(session('success'))
+            <div class="success">{{ session('success') }}</div>
+        @endif
+
+        <form id="formUpdate" action="{{ route('adm.ocorrencia.update', $ocorrencia->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" value="{{ old('nome', $ocorrencia>nome) }}">
+            @error('nome')
+                <div class="error">{{ $message }}</div>
+            @enderror
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" value="{{ old('email', $ocorrencia->email) }}">
+            @error('email')
+                <div class="error">{{ $message }}</div>
+            @enderror
+
+            <label for="telefone">Telefone:</label>
+            <input type="telefone" id="telefone" name="telefone" value="{{ old('telefone', $ocorrencia->telefone) }}">
+            @error('telefone')
+                <div class="error">{{ $message }}</div>
+            @enderror
+
+            <label for="genero">Genero:</label>
+            <select id="genero" name="genero">
+                <option value="">-- Selecione --</option>
+                <option value="Masculino" {{ old('genero', $usuario->genero) == 'Masculino' ? 'selected' : '' }}>Masculino</option>
+                <option value="Feminino" {{ old('genero', $usuario->genero) == 'Feminino' ? 'selected' : '' }}>Feminino</option>
+                <option value="Prefiro Não Informar" {{ old('genero', $usuario->genero) == 'Prefiro Não Informar' ? 'selected' : '' }}>Prefiro Não Informar</option>
+
+            </select>
+            @error('nivelAdmin')
+                <div class="error">{{ $message }}</div>
+            @enderror
+
+            <button type="submit">Alterar</button>
+        </form>
+    </div>
+@endsection
